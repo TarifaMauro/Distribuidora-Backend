a5e52d361cd13510ce190055e99eb5ce3bfad307
Tarifa Mauro Agustín <40440760@fi.unju.edu.ar>
2026-05-04 22:13:07 -0300

    Se agregan configuracion principal en app.js y en db.js.

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+// Conexion a la base de datos
+const pool = require('./config/db');
+
+
+const app = express();
+//Puerto del servidor
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());//Permite que el servidor sea accedido desde cualquier origen
+app.use(express.json());//Permite que el servidor reciba peticiones en formato JSON
+app.use(express.urlencoded({ extended: true }));//Permite que el servidor reciba peticiones en formato URL-encoded
+
+
+
+app.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json({
+            status: 'OK',
+            message: 'Conexion exitosa a la base de datos',
+            databaseTime: result.rows[0].now
+        });
+    } catch (err) {
+        console.error('Error al conectar a la base de datos:', err.stack);
+        res.status(500).json({ error: 'Error al conectar a la base de datos' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
